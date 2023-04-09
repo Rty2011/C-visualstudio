83d580c8b045197e3045e611acca28f88f24140a
류원석노트북 <@DESKTOP-UJ1DAUE>
2023-04-09 20:28:04 +0900

    초기 커밋.

---
+++ 예제2-2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("% c\n", 'A');
+	printf("%s\n", "A");
+	printf("%c은 %s입니다.\n", '1', "first");
+	system("pause");
+	return 0;
+}
+
+// %c= 문자 상수
+// %s= 문자열 상수
